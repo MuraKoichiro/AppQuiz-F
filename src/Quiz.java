4e787ad4dd746f195b9be5943360db752ac731ca
Ise Hayato <hayato.ise.sys@gmail.com>
2017-06-27 11:13:13 +0900

    first commit

---
+++ src/Quiz.java
@@ -0,0 +1,101 @@
+public class Quiz {
+
+	public static void main(String[] args) {
+		//メイン処理
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+	public static int situmon1() {
+		int ans1 = 0;
+		//質問内容
+		//回答内容
+		//ユーザーが答えを入力
+		
+
+
+
+
+
+
+
+
+
+
+		return ans1;
+	}
+
+	public static int situmon2() {
+		int ans2 = 0;
+		//質問内容
+		//回答内容
+	    //ユーザーが答えを入力
+
+
+
+
+
+
+
+
+
+
+
+
+		return ans2;
+	}
+
+	public static int situmon3() {
+		int ans3 = 0;
+		//質問内容
+		//回答内容
+		//ユーザーが答えを入力
+
+
+
+
+
+
+
+
+
+
+
+
+		return ans3;
+	}
+
+	public static int situmon4() {
+		int ans4 = 0;
+		//質問内容
+		//回答内容
+		//ユーザーが答えを入力
+
+
+
+
+
+
+
+
+
+
+
+
+
+		return ans4;
+	}
+
+}
